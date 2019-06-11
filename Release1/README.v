a05afe88ed4d18d37a0d810ff5cb8d48969e5d25
shaun_laptop <40127741+sprice1997@users.noreply.github.com>
2019-06-11 15:45:56 +0100

    Added readme and app inventor project

---
+++ Release1/README.v
@@ -0,0 +1,16 @@
+Main application code is in BLE_SPWAS_R3_CAPVOLTAGE/Application/project_zero.c
+
+Sensor controller files are also in BLE_SPWAS_R3_CAPVOLTAGE/Application:
+scif.h
+scif.c
+scif_framework.c
+scif_framework.h
+scif_osal_tirtos.c
+scif_osal_tirtos.h
+
+Custom BLE profile is in BLE_SPWAS_R3_CAPVOLTAGE/PROFILES:
+spwasService.h
+spwasService.c
+
+androidapp.apk is the application for interfacing to the SPWAS.
+The MIT app inventor code for androidapp.apk can be viewed by opening appInventorProject.aia in MIT app inventor
